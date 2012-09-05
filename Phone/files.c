2f8159fb70b2e40cb93b9a53871a82db473768b2
ZogG <thezogg@gmail.com>
2012-09-06 01:21:23 +0300

    some sorting

--- Phone/files.c
+++ Phone/files.c
@@ -5,23 +5,25 @@ int readContacts(const char* filename, Contacts* phonebook[])
 	int ContactsNum, ContactsPhones, i, j;
 	Contact* contact;
 	Contacts contacts;
+	Position P;
 	
 	//ElementType e;
 	
 	FILE* contactsFP;
 	contactsFP = fopen(filename,"r");
-	
+
 	if (contactsFP)
 	{
 		if(fscanf(contactsFP, "%d",&ContactsNum)==1)
-		  for (i=0;i<26;i++)
-               phonebook[i] = MakeEmpty(NULL);
+			
 		  for (i=0;i<ContactsNum;i++)
 			{
 				contact = (Contact*)malloc(sizeof(Contact));
-				fscanf(contactsFP,"%s\n%s\n%i",&contact->name,&contact->number,&contact->type);
+				fscanf(contactsFP,"%s\n%s\n%d",&contact->name,&contact->number,&contact->type);
 				j = getTheLetter(contact->name[0]);
+				
 				Insert(contact,phonebook[j],phonebook[j]);
+
 			}
 		  fclose(contactsFP);
 		  return 0;
--- Phone/io.c
+++ Phone/io.c
@@ -1,19 +1,148 @@
-#include "io.h"
+﻿#include "io.h"
 
-void printAllContacts(Contacts* phonebook[])
+void printMenu()
+{
+	int choice = NULL;
+	
+	printf("========== Main Menu =============== \n\n");
+	printf("1.Contacts\n");
+	printf("2.Call log \n");
+	printf("3.Message Log \n");
+	printf("4.Close\n\n");
+	choice = getch();
+	switch(choice) {
+		case '1':
+			system("cls");			
+			printMenu2();
+			break;
+		case '2':
+			system("cls");
+			printMenu3();
+			break;
+		case '3':
+			system("cls");
+			printMenu4();
+			break;
+		case '4':
+			//here we need to save all data
+			break;
+		default:
+			system("cls");
+			printMenu();
+	}
+}
+
+void printMenu2()
+{
+	int choice = NULL;
+
+	printf("========== Contacts =============== \n\n");
+	printf("1.Find contact by name \n");
+	printf("2.Find a contact by partial name \n");
+	printf("3.Show all contacts \n");
+	printf("4.Add new contact \n");
+	printf("5.Delete contact \n");
+	printf("6.Return to main menu\n\n");
+	choice = getch();
+	switch(choice) {
+		case '1':
+			printf("1");
+			break;
+		case '2':
+			printf("2");
+			break;
+		case '3':
+			//printAllContacts(phonebook);
+			break;
+		case '4':
+			break;
+		case '5':
+			break;
+		case '6':
+			system("cls");
+			printMenu();
+			break;
+		default:
+			system("cls");
+			printMenu2();
+	}
+}
+
+void printMenu3()
+{
+	int choice = NULL;
+	
+	printf("========== Call Logs =============== \n\n");
+	printf("1.Show all calls\n");
+	printf("2.Add new call \n");
+	printf("3.Delete call \n");
+	printf("4.Return to main menu\n\n");
+	choice = getch();
+	switch(choice) {
+		case '1':
+			printf("1");
+			break;
+		case '2':
+			printf("2");
+			break;
+		case '3':
+			printf("3");
+			break;
+		case '4':
+			system("cls");
+			printMenu();
+			break;
+		default:
+			system("cls");
+			printMenu3();
+	}
+}
+
+void printMenu4()
+{
+	int choice = NULL;
+	
+	printf("========== Message Log =============== \n\n");
+	printf("1.Show recent messages\n");
+	printf("2.Add new message\n");
+	printf("3.Show conversation \n");
+	printf("4.Return to main menu\n\n");
+	choice = getch();
+	switch(choice) {
+		case '1':
+			printf("1");
+			break;
+		case '2':
+			printf("2");
+			break;
+		case '3':
+			printf("3");
+			break;
+		case '4':
+			system("cls");
+			printMenu();
+			break;
+		default:
+			system("cls");
+			printMenu4();
+	}
+}
+
+
+
+void printAllContacts(const Contacts* phonebook[])
 {
 	int i;
 	Position P;
 	Contact* contact;
-	ElementType e;
+	
 	for (i=0;i<LETTERS;i++)
 	{
-		P = Header(phonebook[i]);
-		while (P != NULL)
+		P = First(phonebook[i]);
+		while (P)
 		{
-			e = Retrieve(P);
-			contact = (Contact* )e;
-			printf("%s : %s \n",contact->name, contact->number);
+			contact = (Contact* )Retrieve(P);
+			printf("%s - type:%d - %s \n",contact->name, contact->type, contact->number);
 			P = Advance(P);
 		}
 	}
